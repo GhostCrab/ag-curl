393aefc0ca748a3f7b51398fe7adfdf8f4d128c4
GhostCrab <Ryan.Pielow@gmail.com>
2023-06-23 16:40:50 -0700

    worker test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, HostBinding } from '@angular/core';
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,14 +13,53 @@ export class AppComponent {
   title = 'WBC 2023 Draft Club';
   name = 'NavigationBarProject';
 
-  @HostBinding("class.drawer-open")
+  factorialResult: number = 0;
+  factorialInput: number = 1;
+  testTimer: number = 0;
+
+  @HostBinding('class.drawer-open')
   isDrawerOpen: boolean = false;
-  
+
   toggleDrawer(isDrawerOpen: boolean) {
     this.isDrawerOpen = isDrawerOpen;
   }
-  
+
   constructor() {
-    
+  }
+
+  async calculateFactorialMany() {
+    const start = performance.now();
+    let runners = 0;
+    this.factorialResult = 0;
+    while (this.factorialResult < 50) {
+      if (runners >= 3) {
+        await sleep(20);
+        continue;
+      }
+      
+      runners++;
+      this.calculateFactorial( ({ data }) => {
+        runners--;
+        this.factorialResult++;
+      })
+    }
+
+    const end = performance.now();
+    this.testTimer = end - start;
+  }
+
+  calculateFactorial(done: (this: Worker, ev: MessageEvent<any>) => any) {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./app.worker', import.meta.url));
+      // worker.onmessage = ({ data }) => {
+      //   this.factorialResult = data;
+      // };
+      worker.onmessage = done;
+      worker.postMessage(this.factorialInput);
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
   }
 }
